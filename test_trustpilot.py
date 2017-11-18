b60b0a80ff6a79ecc7163898827a99d745eb77de
zhen.yan@tum.de <zhen.yan@tum.de>
2017-11-18 15:20:08 +0100

    test dict

---
+++ test_trustpilot.py
@@ -0,0 +1,32 @@
+import urllib.request
+
+from bs4 import BeautifulSoup
+
+f=open("data_trustpilot.txt", "a+")
+
+base_url = 'https://de.trustpilot.com/review/flixbus.com?languages=all&page='
+
+for i in range(2):
+    req_url = base_url + str(i + 1)
+    data_i = urllib.request.urlopen(req_url).read()
+    print(i)
+    soup_i = BeautifulSoup(data_i, 'lxml')
+    reviews = soup_i.findAll("div", { "class" : "review-body" })
+    ratings = soup_i.findAll("div", { "class" : "star-rating" })
+    ratings.pop(0)
+    num_ratings = []
+    for r in ratings:
+        num = str(r).find('count-')
+        rate = str(r)[int(num + 6)]
+        num_ratings.append(rate)
+    
+    for i in range(len(reviews)):
+        dict = {}
+        dict['review'] = reviews[i].text
+        dict['stars'] = num_ratings[i]
+        f.write('%s\n' % dict)
+f.close()
+
+#for r in reviews:
+#    print(r.text)
+    #print(r.cssselect('div'))
